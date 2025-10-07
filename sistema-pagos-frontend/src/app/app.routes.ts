37f759b59424ab2759a0b5a99171953fbf910cf5
Andres-leon-fernandez <andres05leonfernandez@gmail.com>
2025-10-07 09:28:28 -0500

    VOLVER HACER ANGULAR

--- sistema-pagos-frontend/src/app/app.routes.ts
+++ sistema-pagos-frontend/src/app/app.routes.ts
@@ -1,21 +1,3 @@
 import { Routes } from '@angular/router';
-import { Home } from './home/home';
-import { Login } from './login/login';
-import { LoadEstudiantes } from './load-estudiantes/load-estudiantes';
-import { LoadPagos } from './load-pagos/load-pagos';
-import { Dashboard } from './dashboard/dashboard';
-import { Estudiantes } from './estudiantes/estudiantes';
-import { Pagos } from './pagos/pagos';
-import { Profile } from './profile/profile';
 
-
-export const routes: Routes = [
-    { path: "home", component: Home },
-    { path: "profile", component: Profile },
-    { path: "login", component:Login },
-    { path: "loadEstudiantes", component: LoadEstudiantes },
-    { path: "loadPagos", component: LoadPagos },
-    {path:"dashboard",component:Dashboard},
-    {path:"estudiantes", component:Estudiantes},
-    {path:"pagos",component:Pagos}
-];
+export const routes: Routes = [];
--- sistema-pagos-frontend/src/app/app.ts
+++ sistema-pagos-frontend/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AdminTemplate } from "./admin-template/admin-template";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AdminTemplate],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('sistema-pagos-frontend');
