abc26c08ce93d61459fd473b82b0c833e434ab85
Andrés León <andres05leonfernandez@gmail.com>
2025-10-13 15:13:28 -0500

    falta completar

--- sistema-pagos-frontend/src/app/layout/estudiantes/estudiantes.ts
+++ sistema-pagos-frontend/src/app/layout/estudiantes/estudiantes.ts
@@ -1,29 +1,34 @@
+import { HttpClient } from '@angular/common/http';
 import { EstudiantesService } from './../../services/estudiantes-service';
 import { Estudiante } from '../../models/estudiantes.models';
 import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule } from '@angular/material/divider';
 import { NgForOf } from '@angular/common';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
 @Component({
   selector: 'app-estudiantes',
-  imports: [MatCardModule, MatDividerModule, NgForOf],
+  imports: [MatCardModule, MatDividerModule, NgForOf,MatSortModule,MatTableModule],
   templateUrl: './estudiantes.html',
-  styleUrl: './estudiantes.css'
+  styleUrl: './estudiantes.css',
 })
-export class Estudiantes implements OnInit{
+export class Estudiantes implements OnInit {
+  estudiantes!: Array<Estudiante>;
+  estudiantesDataSource!: any;
+  displayedColumns: string[] = ['id', 'nombre', 'apellido', 'codigo', 'programaId'];
 
-  estudiantes !: Array<Estudiante>;
-
-  constructor(private estudiantesService:EstudiantesService){}
+  constructor(private estudiantesService: EstudiantesService, private httpClient: HttpClient) {}
 
   ngOnInit(): void {
     this.estudiantesService.getAllEstudiantes().subscribe({
-      next: value=>{
-        this.estudiantes=value;
+      next: (value) => {
+        this.estudiantes = value;
+        this.estudiantesDataSource = new MatTableDataSource(this.estudiantes);
       },
-      error:err=>{
+      error: (err) => {
         console.log(err);
-      }
-    })
+      },
+    });
   }
 }
